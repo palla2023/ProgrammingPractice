0155396d05c7349c004063a9e76e20ee29d699f2
BHANUCHANDRA <pallabhanuchandra@gmail.com>
2018-10-10 10:53:52 +0530

    It solves the problem to detect number of swaps in array

--- BubbleSort.java
+++ BubbleSort.java
@@ -11,6 +11,7 @@ public class BubbleSort
         {
             int n = sc.nextInt();
             int a[] = new int[n];
+
             for (int j=0;j<n;j++)
                 a[j] = sc.nextInt();
             for (int j=0;j<n;j++)
