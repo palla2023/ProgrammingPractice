34542dd4bf0f7f9d43a7c6c7d2abaf64d0377594
BHANUCHANDRA <pallabhanuchandra@gmail.com>
2018-10-10 17:53:51 +0530

    Final

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,18 +4,22 @@ public class BubbleSort
 {
     public static void main(String[] args)
     {
-        int swap,count=0;
+        int swap,count,swapped = 0;
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+
+        //For no.of Test cases
         for (int i=0;i<t;i++)
         {
             int n = sc.nextInt();
             int a[] = new int[n];
             for (int j=0;j<n;j++)
                 a[j] = sc.nextInt();
+            count  = 0;
+            //For Sorting the elements by using Bubble Sort Algorithm
             for (int j=0;j<n;j++)
             {
-                int swapped = 0;
+
                 for (int k=0;k<n-1;k++)
                 {
                     if (a[k]>a[k+1])
@@ -27,11 +31,20 @@ public class BubbleSort
                         swapped=1;
                     }
                 }
+                //For checking the elements are need to be sort or already in sorted order
                 if (swapped==0)
                     break;
             }
             System.out.println();
-            System.out.print(count);
+            //Printing the sorted array
+            /*for (int k=0;k<n;k++)
+                System.out.print(a[k]+" ");*/
+
+            //Printing no.of swaps
+            if(swapped==0)
+                System.out.print("0");
+            else
+                System.out.print(count);
         }
     }
 }
